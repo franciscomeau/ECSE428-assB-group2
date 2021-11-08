24fcc646ce89cd85fdfb7edd863d327f43d18fe9
Francis Comeau <francis.comeau@mail.mcgill.ca>
2021-11-08 14:14:23 -0500

    step 3 of calculator tutorial: main py file

---
+++ postage_flask/postage.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+Flask_App = Flask(__name__) # Creating our Flask Instance
+
+@Flask_App.route('/', methods=['GET'])
+def index():
+    """ Displays the index page accessible at '/' """
+
+    return render_template('index.html')
+
+@Flask_App.route('/operation_result/'. methods=['POST'])
+def operation_result():
+    """Route where we send calculator form input"""
+    return (render_template, 'index.html')
+
+if __name__ == '__main__':
+    Flask_App.debug = True
+    Flask_App.run()
